361c98b995cd697f52adc6e3eae1058d56b7f518
Purvi Kanal <purvikanal@honeycomb.io>
2023-05-10 15:47:26 -0400

    base example wip

---
+++ examples/instrumentations/document-load/index.js
@@ -0,0 +1,5 @@
+import { registerInstrumentations } from '@opentelemetry/sandbox-instrumentation';
+import { DocumentLoadInstrumentation } from '@opentelemetry/sandbox-instrumentation-document-load';
+console.log(DocumentLoadInstrumentation);
+console.log(registerInstrumentations);
+console.log('hello!');
