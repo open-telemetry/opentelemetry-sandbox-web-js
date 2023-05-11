378e8eafe8054e0a4abe70ecf4829ee8f753baf5
Purvi Kanal <purvikanal@honeycomb.io>
2023-05-11 16:05:06 -0400

    add proper document load example

--- examples/instrumentations/examples/document-load/index.js
+++ examples/instrumentations/examples/document-load/index.js
@@ -1,5 +1,21 @@
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-web';
 import { registerInstrumentations } from '@opentelemetry/sandbox-instrumentation';
 import { DocumentLoadInstrumentation } from '@opentelemetry/sandbox-instrumentation-document-load';
-console.log(DocumentLoadInstrumentation);
-console.log(registerInstrumentations);
-console.log('hello!');
+import { ZoneContextManager } from '@opentelemetry/sandbox-context-zone';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+
+const provider = new WebTracerProvider();
+
+// Note: For production consider using the "BatchSpanProcessor" to reduce the number of requests
+// to your exporter. Using the SimpleSpanProcessor here as it sends the spans immediately to the
+// exporter without delay
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+registerInstrumentations({
+  instrumentations: [
+    new DocumentLoadInstrumentation()
+  ]
+});
