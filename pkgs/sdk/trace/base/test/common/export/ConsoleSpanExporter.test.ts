10f6f9545d78fed45165349a38dbcc4cb4857e2d
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2023-07-26 09:58:21 -0700

    [AutoMerge] staging @ [3a875dd...](https://github.com/open-telemetry/opentelemetry-sandbox-web-js/commit/3a875ddd05150e10093e7d5e9aa57310a8f8937d)
    ### Moving package from auto-merge/js/api to pkgs/api/
    ### Moving package from auto-merge/js/packages/opentelemetry-semantic-conventions/ to pkgs/semantic-conventions/
    ### Moving package from auto-merge/js/packages/opentelemetry-core/ to pkgs/core/
    ### Moving package from auto-merge/js/packages/opentelemetry-resources/ to pkgs/resources/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone-peer-dep/ to pkgs/context/zone-peer-dep/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone/ to pkgs/context/zone/
    ### Moving package from auto-merge/js/packages/opentelemetry-propagator-b3/ to pkgs/propagators/b3/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-base/ to pkgs/sdk/trace/base/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-web/ to pkgs/sdk/trace/web/
    ### Moving package from auto-merge/js/packages/sdk-metrics/ to pkgs/sdk/metrics/
    ### Moving package from auto-merge/js/experimental/packages/sdk-logs/ to pkgs/sdk/logs/
    ### Moving package from auto-merge/js/experimental/packages/otlp-exporter-base to pkgs/exporters/otlp/base/
    ### Moving package from auto-merge/js/experimental/packages/otlp-transformer to pkgs/exporters/otlp/transformer
    ### Moving package from auto-merge/js/experimental/packages/otlp-proto-exporter-base to pkgs/exporters/otlp/proto-base
    ### Moving package from auto-merge/js/packages/opentelemetry-exporter-zipkin/ to pkgs/exporters/zipkin/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-proto to pkgs/exporters/otlp/trace/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-http to pkgs/exporters/otlp/trace/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-exporter-metrics-otlp-http to pkgs/exporters/otlp/metrics/http/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-proto to pkgs/exporters/otlp/logs/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-http to pkgs/exporters/otlp/logs/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation/ to pkgs/instrumentations/instrumentation/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-fetch/ to pkgs/instrumentations/web/fetch
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-xml-http-request/ to pkgs/instrumentations/web/xhr
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-document-load/ to pkgs/instrumentations/web/document-load/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-user-interaction/ to pkgs/instrumentations/web/user-interaction/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-long-task/ to pkgs/instrumentations/web/long-task/
    ### Moving package from auto-merge/contrib/metapackages/auto-instrumentations-web/ to pkgs/instrumentations/web/auto/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-browser-detector/ to pkgs/detectors/browser/
    ### Moving package from auto-merge/js/experimental/packages/api-logs/ to pkgs/api-logs/
    ### Moving package from auto-merge/js/experimental/packages/api-events/ to pkgs/api-events/
    ### Moving package from auto-merge/js/examples/opentelemetry-web/ to examples/web/js/

---
+++ pkgs/sdk/trace/base/test/common/export/ConsoleSpanExporter.test.ts
@@ -0,0 +1,109 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { SpanContext, TraceFlags } from '@opentelemetry/sandbox-api';
+import { TraceState } from '@opentelemetry/sandbox-core';
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import {
+  AlwaysOnSampler,
+  BasicTracerProvider,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '../../../src';
+
+/* eslint-disable no-console */
+describe('ConsoleSpanExporter', () => {
+  let consoleExporter: ConsoleSpanExporter;
+  let previousConsoleDir: any;
+
+  beforeEach(() => {
+    previousConsoleDir = console.dir;
+    console.dir = () => {};
+    consoleExporter = new ConsoleSpanExporter();
+  });
+
+  afterEach(() => {
+    console.dir = previousConsoleDir;
+  });
+
+  describe('.export()', () => {
+    it('should export information about span', () => {
+      assert.doesNotThrow(() => {
+        const basicTracerProvider = new BasicTracerProvider({
+          sampler: new AlwaysOnSampler(),
+        });
+        consoleExporter = new ConsoleSpanExporter();
+
+        const spyConsole = sinon.spy(console, 'dir');
+        const spyExport = sinon.spy(consoleExporter, 'export');
+
+        basicTracerProvider.addSpanProcessor(
+          new SimpleSpanProcessor(consoleExporter)
+        );
+
+        const tracer = basicTracerProvider.getTracer('default');
+        const context: SpanContext = {
+          traceId: 'a3cda95b652f4a1592b449d5929fda1b',
+          spanId: '5e0c63257de34c92',
+          traceFlags: TraceFlags.SAMPLED,
+        };
+        const span = tracer.startSpan('foo', {
+          links: [{ context, attributes: { anAttr: 'aValue' } }],
+        });
+        span.spanContext().traceState = new TraceState('trace=state');
+        span.addEvent('foobar');
+        span.end();
+
+        const spans = spyExport.args[0];
+        const firstSpan = spans[0][0];
+        const firstEvent = firstSpan.events[0];
+        const consoleArgs = spyConsole.args[0];
+        const consoleSpan = consoleArgs[0];
+        const keys = Object.keys(consoleSpan).sort().join(',');
+
+        const expectedKeys = [
+          'attributes',
+          'duration',
+          'events',
+          'id',
+          'kind',
+          'links',
+          'name',
+          'parentId',
+          'status',
+          'timestamp',
+          'traceId',
+          'traceState',
+        ].join(',');
+
+        assert.ok(firstSpan.name === 'foo');
+        assert.ok(firstEvent.name === 'foobar');
+        assert.ok(consoleSpan.id === firstSpan.spanContext().spanId);
+        assert.ok(keys === expectedKeys, 'expectedKeys');
+
+        assert.ok(spyExport.calledOnce);
+      });
+    });
+  });
+
+  describe('force flush', () => {
+    it('forceFlush should flush spans and return', async () => {
+      consoleExporter = new ConsoleSpanExporter();
+      await consoleExporter.forceFlush();
+    });
+  });
+});
