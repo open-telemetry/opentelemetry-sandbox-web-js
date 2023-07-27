08008c8af59ade7e851c83550f6d9e53c9cefaa9
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2023-07-26 17:21:58 -0700

    [AutoMerge] staging @ [3a875dd...](https://github.com/open-telemetry/opentelemetry-sandbox-web-js/commit/3a875ddd05150e10093e7d5e9aa57310a8f8937d)
    ### Moving package from auto-merge/js/api to pkgs/api/
    ### Moving package from auto-merge/js/packages/opentelemetry-semantic-conventions/ to pkgs/semantic-conventions/
    ### Moving package from auto-merge/js/packages/opentelemetry-core/ to pkgs/core/
    ### Moving package from auto-merge/js/packages/opentelemetry-resources/ to pkgs/resources/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone-peer-dep/ to pkgs/context/zone-peer-dep/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone/ to pkgs/context/zone/
    ### Moving package from auto-merge/js/packages/opentelemetry-propagator-b3/ to pkgs/propagators/b3/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-base/ to pkgs/sdk/trace/base/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-web/ to pkgs/sdk/trace/web/
    ### Moving package from auto-merge/js/packages/sdk-metrics/ to pkgs/sdk/metrics/
    ### Moving package from auto-merge/js/experimental/packages/sdk-logs/ to pkgs/sdk/logs/
    ### Moving package from auto-merge/js/experimental/packages/otlp-exporter-base to pkgs/exporters/otlp/base/
    ### Moving package from auto-merge/js/experimental/packages/otlp-transformer to pkgs/exporters/otlp/transformer
    ### Moving package from auto-merge/js/experimental/packages/otlp-proto-exporter-base to pkgs/exporters/otlp/proto-base
    ### Moving package from auto-merge/js/packages/opentelemetry-exporter-zipkin/ to pkgs/exporters/zipkin/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-proto to pkgs/exporters/otlp/trace/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-http to pkgs/exporters/otlp/trace/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-exporter-metrics-otlp-http to pkgs/exporters/otlp/metrics/http/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-proto to pkgs/exporters/otlp/logs/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-http to pkgs/exporters/otlp/logs/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation/ to pkgs/instrumentations/instrumentation/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-fetch/ to pkgs/instrumentations/web/fetch
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-xml-http-request/ to pkgs/instrumentations/web/xhr
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-document-load/ to pkgs/instrumentations/web/document-load/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-user-interaction/ to pkgs/instrumentations/web/user-interaction/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-long-task/ to pkgs/instrumentations/web/long-task/
    ### Moving package from auto-merge/contrib/metapackages/auto-instrumentations-web/ to pkgs/instrumentations/web/auto/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-browser-detector/ to pkgs/detectors/browser/
    ### Moving package from auto-merge/js/experimental/packages/api-logs/ to pkgs/api-logs/
    ### Moving package from auto-merge/js/experimental/packages/api-events/ to pkgs/api-events/
    ### Moving package from auto-merge/js/examples/opentelemetry-web/ to examples/web/js/

---
+++ pkgs/instrumentations/instrumentation/test/common/autoLoader.test.ts
@@ -0,0 +1,124 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  Tracer,
+  TracerProvider,
+  Meter,
+  MeterOptions,
+  MeterProvider,
+} from '@opentelemetry/sandbox-api';
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import { InstrumentationBase, registerInstrumentations } from '../../src';
+
+class DummyTracerProvider implements TracerProvider {
+  getTracer(name: string, version?: string): Tracer {
+    throw new Error('not implemented');
+  }
+}
+
+class DummyMeterProvider implements MeterProvider {
+  getMeter(name: string, version?: string, options?: MeterOptions): Meter {
+    throw new Error('not implemented');
+  }
+}
+
+class FooInstrumentation extends InstrumentationBase {
+  init() {
+    return [];
+  }
+  override enable() {}
+  override disable() {}
+}
+
+describe('autoLoader', () => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  let unload: Function | undefined;
+
+  afterEach(() => {
+    sinon.restore();
+    if (typeof unload === 'function') {
+      unload();
+      unload = undefined;
+    }
+  });
+
+  describe('registerInstrumentations', () => {
+    describe('InstrumentationBase', () => {
+      let instrumentation: InstrumentationBase;
+      let enableSpy: sinon.SinonSpy;
+      let setTracerProviderSpy: sinon.SinonSpy;
+      let setMeterProviderSpy: sinon.SinonSpy;
+      const tracerProvider = new DummyTracerProvider();
+      const meterProvider = new DummyMeterProvider();
+      beforeEach(() => {
+        instrumentation = new FooInstrumentation('foo', '1', {});
+        enableSpy = sinon.spy(instrumentation, 'enable');
+        setTracerProviderSpy = sinon.stub(instrumentation, 'setTracerProvider');
+        setMeterProviderSpy = sinon.stub(instrumentation, 'setMeterProvider');
+        unload = registerInstrumentations({
+          instrumentations: [instrumentation],
+          tracerProvider,
+          meterProvider,
+        });
+      });
+
+      afterEach(() => {
+        Object.keys(require.cache).forEach(key => delete require.cache[key]);
+        if (typeof unload === 'function') {
+          unload();
+          unload = undefined;
+        }
+      });
+
+      it('should enable disabled instrumentation', () => {
+        if (typeof unload === 'function') {
+          unload();
+          unload = undefined;
+        }
+        instrumentation = new FooInstrumentation('foo', '1', {
+          enabled: false,
+        });
+        enableSpy = sinon.spy(instrumentation, 'enable');
+        setTracerProviderSpy = sinon.stub(instrumentation, 'setTracerProvider');
+        setMeterProviderSpy = sinon.stub(instrumentation, 'setMeterProvider');
+        unload = registerInstrumentations({
+          instrumentations: [instrumentation],
+          tracerProvider,
+          meterProvider,
+        });
+        assert.strictEqual(enableSpy.callCount, 1);
+      });
+
+      it('should NOT enable enabled instrumentation', () => {
+        assert.strictEqual(enableSpy.callCount, 0);
+      });
+
+      it('should set TracerProvider', () => {
+        assert.strictEqual(setTracerProviderSpy.callCount, 1);
+        assert.ok(setTracerProviderSpy.lastCall.args[0] === tracerProvider);
+        assert.strictEqual(setTracerProviderSpy.lastCall.args.length, 1);
+      });
+
+      it('should set MeterProvider', () => {
+        assert.strictEqual(setMeterProviderSpy.callCount, 1);
+        assert.ok(setMeterProviderSpy.lastCall.args[0] === meterProvider);
+        assert.strictEqual(setMeterProviderSpy.lastCall.args.length, 1);
+      });
+    });
+  });
+});
