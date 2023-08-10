600bf22634190d38ad72edbc673a31e777d180eb
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2023-08-10 14:05:17 -0700

    [AutoMerge] staging @ [44bcf14...](https://github.com/open-telemetry/opentelemetry-sandbox-web-js/commit/44bcf146378abd92bed9e6fd995cf3fc11063987)
    ### Moving package from auto-merge/js/api to pkgs/api/
    ### Moving package from auto-merge/js/packages/opentelemetry-semantic-conventions/ to pkgs/semantic-conventions/
    ### Moving package from auto-merge/js/packages/opentelemetry-core/ to pkgs/core/
    ### Moving package from auto-merge/js/packages/opentelemetry-resources/ to pkgs/resources/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone-peer-dep/ to pkgs/context/zone-peer-dep/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone/ to pkgs/context/zone/
    ### Moving package from auto-merge/js/packages/opentelemetry-propagator-b3/ to pkgs/propagators/b3/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-base/ to pkgs/sdk/trace/base/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-web/ to pkgs/sdk/trace/web/
    ### Moving package from auto-merge/js/packages/sdk-metrics/ to pkgs/sdk/metrics/
    ### Moving package from auto-merge/js/experimental/packages/sdk-logs/ to pkgs/sdk/logs/
    ### Moving package from auto-merge/js/experimental/packages/otlp-exporter-base to pkgs/exporters/otlp/base/
    ### Moving package from auto-merge/js/experimental/packages/otlp-transformer to pkgs/exporters/otlp/transformer
    ### Moving package from auto-merge/js/experimental/packages/otlp-proto-exporter-base to pkgs/exporters/otlp/proto-base
    ### Moving package from auto-merge/js/packages/opentelemetry-exporter-zipkin/ to pkgs/exporters/zipkin/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-proto to pkgs/exporters/otlp/trace/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-http to pkgs/exporters/otlp/trace/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-exporter-metrics-otlp-http to pkgs/exporters/otlp/metrics/http/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-proto to pkgs/exporters/otlp/logs/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-http to pkgs/exporters/otlp/logs/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation/ to pkgs/instrumentations/instrumentation/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-fetch/ to pkgs/instrumentations/web/fetch
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-xml-http-request/ to pkgs/instrumentations/web/xhr
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-document-load/ to pkgs/instrumentations/web/document-load/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-user-interaction/ to pkgs/instrumentations/web/user-interaction/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-long-task/ to pkgs/instrumentations/web/long-task/
    ### Moving package from auto-merge/contrib/metapackages/auto-instrumentations-web/ to pkgs/instrumentations/web/auto/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-browser-detector/ to pkgs/detectors/browser/
    ### Moving package from auto-merge/js/experimental/packages/api-logs/ to pkgs/api-logs/
    ### Moving package from auto-merge/js/experimental/packages/api-events/ to pkgs/api-events/
    ### Moving package from auto-merge/js/examples/opentelemetry-web/ to examples/web/js/

---
+++ pkgs/instrumentations/instrumentation/test/common/Instrumentation.test.ts
@@ -0,0 +1,117 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as assert from 'assert';
+import {
+  Instrumentation,
+  InstrumentationBase,
+  InstrumentationConfig,
+} from '../../src';
+
+import { MeterProvider } from '@opentelemetry/sandbox-sdk-metrics';
+
+interface TestInstrumentationConfig extends InstrumentationConfig {
+  isActive?: boolean;
+}
+
+class TestInstrumentation extends InstrumentationBase {
+  constructor(config: TestInstrumentationConfig & InstrumentationConfig = {}) {
+    super('test', '1.0.0', Object.assign({}, config));
+  }
+  override enable() {}
+  override disable() {}
+  init() {}
+}
+
+describe('BaseInstrumentation', () => {
+  let instrumentation: Instrumentation;
+  beforeEach(() => {
+    instrumentation = new TestInstrumentation();
+  });
+
+  it('should create an instance', () => {
+    assert.ok(instrumentation instanceof InstrumentationBase);
+  });
+
+  it('should have a name', () => {
+    assert.deepStrictEqual(instrumentation.instrumentationName, 'test');
+  });
+
+  it('should have a version', () => {
+    assert.deepStrictEqual(instrumentation.instrumentationVersion, '1.0.0');
+  });
+
+  describe('constructor', () => {
+    it('should enable instrumentation by default', () => {
+      let enableCalled = false;
+      let updateMetricInstrumentsCalled = false;
+      class TestInstrumentation2 extends TestInstrumentation {
+        override enable() {
+          enableCalled = true;
+        }
+        override _updateMetricInstruments() {
+          updateMetricInstrumentsCalled = true;
+        }
+      }
+      instrumentation = new TestInstrumentation2();
+      assert.strictEqual(enableCalled, true);
+      assert.strictEqual(updateMetricInstrumentsCalled, true);
+    });
+  });
+
+  describe('setMeterProvider', () => {
+    let otelTestingMeterProvider: MeterProvider;
+    beforeEach(() => {
+      otelTestingMeterProvider = new MeterProvider();
+    });
+    it('should call _updateMetricInstruments', () => {
+      let called = true;
+      class TestInstrumentation2 extends TestInstrumentation {
+        override _updateMetricInstruments() {
+          called = true;
+        }
+      }
+      instrumentation = new TestInstrumentation2();
+      instrumentation.setMeterProvider(otelTestingMeterProvider);
+      assert.strictEqual(called, true);
+    });
+  });
+
+  describe('getConfig', () => {
+    it('should return instrumentation config', () => {
+      const instrumentation: Instrumentation = new TestInstrumentation({
+        isActive: false,
+      });
+      const configuration =
+        instrumentation.getConfig() as TestInstrumentationConfig;
+      assert.notStrictEqual(configuration, null);
+      assert.strictEqual(configuration.isActive, false);
+    });
+  });
+
+  describe('setConfig', () => {
+    it('should set a new config for instrumentation', () => {
+      const instrumentation: Instrumentation = new TestInstrumentation();
+      const config: TestInstrumentationConfig = {
+        isActive: true,
+      };
+      instrumentation.setConfig(config);
+      const configuration =
+        instrumentation.getConfig() as TestInstrumentationConfig;
+      assert.strictEqual(configuration.isActive, true);
+    });
+  });
+});
