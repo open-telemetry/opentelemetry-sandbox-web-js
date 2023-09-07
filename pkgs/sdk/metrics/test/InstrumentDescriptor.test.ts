1d74b07a0de5e2aaccde382157f77bd0e051ef1a
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2023-09-07 17:17:14 +0000

    [AutoMerge] staging @ [116dea1...](https://github.com/open-telemetry/opentelemetry-sandbox-web-js/commit/116dea1946fc8a9b65e9f2efe0980399f52eb1f7)
    ### Moving package from auto-merge/js/api to pkgs/api/
    ### Moving package from auto-merge/js/packages/opentelemetry-semantic-conventions/ to pkgs/semantic-conventions/
    ### Moving package from auto-merge/js/packages/opentelemetry-core/ to pkgs/core/
    ### Moving package from auto-merge/js/packages/opentelemetry-resources/ to pkgs/resources/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone-peer-dep/ to pkgs/context/zone-peer-dep/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone/ to pkgs/context/zone/
    ### Moving package from auto-merge/js/packages/opentelemetry-propagator-b3/ to pkgs/propagators/b3/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-base/ to pkgs/sdk/trace/base/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-web/ to pkgs/sdk/trace/web/
    ### Moving package from auto-merge/js/packages/sdk-metrics/ to pkgs/sdk/metrics/
    ### Moving package from auto-merge/js/experimental/packages/sdk-logs/ to pkgs/sdk/logs/
    ### Moving package from auto-merge/js/experimental/packages/otlp-exporter-base to pkgs/exporters/otlp/base/
    ### Moving package from auto-merge/js/experimental/packages/otlp-transformer to pkgs/exporters/otlp/transformer
    ### Moving package from auto-merge/js/experimental/packages/otlp-proto-exporter-base to pkgs/exporters/otlp/proto-base
    ### Moving package from auto-merge/js/packages/opentelemetry-exporter-zipkin/ to pkgs/exporters/zipkin/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-proto to pkgs/exporters/otlp/trace/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-http to pkgs/exporters/otlp/trace/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-exporter-metrics-otlp-http to pkgs/exporters/otlp/metrics/http/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-proto to pkgs/exporters/otlp/logs/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-http to pkgs/exporters/otlp/logs/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation/ to pkgs/instrumentations/instrumentation/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-fetch/ to pkgs/instrumentations/web/fetch
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-xml-http-request/ to pkgs/instrumentations/web/xhr
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-document-load/ to pkgs/instrumentations/web/document-load/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-user-interaction/ to pkgs/instrumentations/web/user-interaction/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-long-task/ to pkgs/instrumentations/web/long-task/
    ### Moving package from auto-merge/contrib/metapackages/auto-instrumentations-web/ to pkgs/instrumentations/web/auto/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-browser-detector/ to pkgs/detectors/browser/
    ### Moving package from auto-merge/js/experimental/packages/api-logs/ to pkgs/api-logs/
    ### Moving package from auto-merge/js/experimental/packages/api-events/ to pkgs/api-events/
    ### Moving package from auto-merge/js/examples/opentelemetry-web/ to examples/web/js/

---
+++ pkgs/sdk/metrics/test/InstrumentDescriptor.test.ts
@@ -0,0 +1,144 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as assert from 'assert';
+import {
+  createInstrumentDescriptor,
+  InstrumentDescriptor,
+  InstrumentType,
+  isValidName,
+  isDescriptorCompatibleWith,
+} from '../src/InstrumentDescriptor';
+import { invalidNames, validNames } from './util';
+import { ValueType } from '@opentelemetry/sandbox-api';
+
+describe('InstrumentDescriptor', () => {
+  describe('createInstrumentDescriptor', () => {
+    for (const val of [null, undefined]) {
+      it(`should interpret an empty unit value as a blank string (${val})`, () => {
+        const result = createInstrumentDescriptor(
+          'example',
+          InstrumentType.COUNTER,
+          {
+            unit: val as any,
+          }
+        );
+        assert.strictEqual(result.unit, '');
+      });
+    }
+  });
+
+  describe('isDescriptorCompatibleWith', () => {
+    const tests: [
+      string,
+      boolean,
+      InstrumentDescriptor,
+      InstrumentDescriptor,
+    ][] = [
+      [
+        'Compatible with identical descriptors',
+        true,
+        {
+          name: 'foo',
+          description: 'any descriptions',
+          unit: 'kB',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+        {
+          name: 'foo',
+          description: 'any descriptions',
+          unit: 'kB',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+      ],
+      [
+        'Compatible with case-insensitive names',
+        true,
+        {
+          name: 'foo',
+          description: '',
+          unit: '',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+        {
+          name: 'FoO',
+          description: '',
+          unit: '',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+      ],
+      [
+        'Incompatible with different names',
+        false,
+        {
+          name: 'foo',
+          description: '',
+          unit: '',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+        {
+          name: 'foobar',
+          description: '',
+          unit: '',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+      ],
+      [
+        'Incompatible with case-sensitive units',
+        false,
+        {
+          name: 'foo',
+          description: '',
+          unit: 'kB',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+        {
+          name: 'foo',
+          description: '',
+          unit: 'kb',
+          type: InstrumentType.COUNTER,
+          valueType: ValueType.DOUBLE,
+        },
+      ],
+    ];
+    for (const test of tests) {
+      it(test[0], () => {
+        assert.ok(isDescriptorCompatibleWith(test[2], test[3]) === test[1]);
+      });
+    }
+  });
+
+  describe('isValidName', () => {
+    it('should validate names', () => {
+      for (const invalidName of invalidNames) {
+        assert.ok(
+          !isValidName(invalidName),
+          `${invalidName} should be invalid`
+        );
+      }
+      for (const validName of validNames) {
+        assert.ok(isValidName(validName), `${validName} should be valid`);
+      }
+    });
+  });
+});
---
+++ pkgs/sdk/trace/web/test/utils.test.ts
@@ -0,0 +1,519 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  hrTimeToNanoseconds,
+  otperformance as performance,
+} from '@opentelemetry/sandbox-core';
+import * as core from '@opentelemetry/sandbox-core';
+import * as tracing from '@opentelemetry/sandbox-sdk-trace-base';
+import { HrTime } from '@opentelemetry/sandbox-api';
+
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import {
+  addSpanNetworkEvent,
+  addSpanNetworkEvents,
+  getResource,
+  normalizeUrl,
+  parseUrl,
+  PerformanceEntries,
+  shouldPropagateTraceHeaders,
+  URLLike,
+} from '../src';
+import { PerformanceTimingNames as PTN } from '../src/enums/PerformanceTimingNames';
+
+const SECOND_TO_NANOSECONDS = 1e9;
+
+function createHrTime(startTime: HrTime, addToStart: number): HrTime {
+  let seconds = startTime[0];
+  let nanos = startTime[1] + addToStart;
+  if (nanos >= SECOND_TO_NANOSECONDS) {
+    nanos = SECOND_TO_NANOSECONDS - nanos;
+    seconds++;
+  }
+  return [seconds, nanos];
+}
+
+function createResource(
+  resource = {},
+  startTime: HrTime,
+  addToStart: number
+): PerformanceResourceTiming {
+  const fetchStart = core.hrTimeToNanoseconds(startTime) + 1;
+  const responseEnd = fetchStart + addToStart;
+  const million = 1000 * 1000; // used to convert nano to milli
+  const defaultResource = {
+    connectEnd: 0,
+    connectStart: 0,
+    decodedBodySize: 0,
+    domainLookupEnd: 0,
+    domainLookupStart: 0,
+    encodedBodySize: 0,
+    fetchStart: fetchStart / million,
+    initiatorType: 'xmlhttprequest',
+    nextHopProtocol: '',
+    redirectEnd: 0,
+    redirectStart: 0,
+    requestStart: 0,
+    responseEnd: responseEnd / million,
+    responseStart: 0,
+    secureConnectionStart: 0,
+    transferSize: 0,
+    workerStart: 0,
+    duration: 0,
+    entryType: '',
+    name: '',
+    startTime: 0,
+  };
+  return Object.assign(
+    {},
+    defaultResource,
+    resource
+  ) as PerformanceResourceTiming;
+}
+
+describe('utils', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('addSpanNetworkEvents', () => {
+    it('should add all network events to span', () => {
+      const addEventSpy = sinon.spy();
+      const setAttributeSpy = sinon.spy();
+      const span = {
+        addEvent: addEventSpy,
+        setAttribute: setAttributeSpy,
+      } as unknown as tracing.Span;
+      const entries = {
+        [PTN.FETCH_START]: 123,
+        [PTN.DOMAIN_LOOKUP_START]: 123,
+        [PTN.DOMAIN_LOOKUP_END]: 123,
+        [PTN.CONNECT_START]: 123,
+        [PTN.SECURE_CONNECTION_START]: 123,
+        [PTN.CONNECT_END]: 123,
+        [PTN.REQUEST_START]: 123,
+        [PTN.RESPONSE_START]: 123,
+        [PTN.RESPONSE_END]: 123,
+        [PTN.DECODED_BODY_SIZE]: 123,
+        [PTN.ENCODED_BODY_SIZE]: 61,
+      } as PerformanceEntries;
+
+      assert.strictEqual(addEventSpy.callCount, 0);
+
+      addSpanNetworkEvents(span, entries);
+      assert.strictEqual(setAttributeSpy.callCount, 2);
+      //secure connect start should not be added to non-https resource
+      assert.strictEqual(addEventSpy.callCount, 8);
+      //secure connect start should be added to an https resource
+      addEventSpy.resetHistory();
+      addSpanNetworkEvents(span, {
+        ...entries,
+        name: 'https://foo',
+      } as PerformanceResourceTiming);
+      assert.strictEqual(addEventSpy.callCount, 9);
+    });
+    it('should only include encoded size when content encoding is being used', () => {
+      const addEventSpy = sinon.spy();
+      const setAttributeSpy = sinon.spy();
+      const span = {
+        addEvent: addEventSpy,
+        setAttribute: setAttributeSpy,
+      } as unknown as tracing.Span;
+      const entries = {
+        [PTN.DECODED_BODY_SIZE]: 123,
+        [PTN.ENCODED_BODY_SIZE]: 123,
+      } as PerformanceEntries;
+
+      assert.strictEqual(setAttributeSpy.callCount, 0);
+
+      addSpanNetworkEvents(span, entries);
+
+      assert.strictEqual(addEventSpy.callCount, 0);
+      assert.strictEqual(setAttributeSpy.callCount, 1);
+    });
+  });
+  describe('addSpanNetworkEvent', () => {
+    [0, -2, 123].forEach(value => {
+      describe(`when entry is ${value}`, () => {
+        it('should add event to span', () => {
+          const addEventSpy = sinon.spy();
+          const span = {
+            addEvent: addEventSpy,
+          } as unknown as tracing.Span;
+          const entries = {
+            [PTN.FETCH_START]: value,
+          } as PerformanceEntries;
+
+          assert.strictEqual(addEventSpy.callCount, 0);
+
+          addSpanNetworkEvent(span, PTN.FETCH_START, entries);
+
+          assert.strictEqual(addEventSpy.callCount, 1);
+          const args = addEventSpy.args[0];
+
+          assert.strictEqual(args[0], 'fetchStart');
+          assert.strictEqual(args[1], value);
+        });
+      });
+    });
+    describe('when entry is not numeric', () => {
+      it('should NOT add event to span', () => {
+        const addEventSpy = sinon.spy();
+        const span = {
+          addEvent: addEventSpy,
+        } as unknown as tracing.Span;
+        const entries = {
+          [PTN.FETCH_START]: 'non-numeric',
+        } as unknown;
+
+        assert.strictEqual(addEventSpy.callCount, 0);
+
+        addSpanNetworkEvent(
+          span,
+          PTN.FETCH_START,
+          entries as PerformanceEntries
+        );
+
+        assert.strictEqual(addEventSpy.callCount, 0);
+      });
+    });
+    describe('when entries does NOT contain the performance', () => {
+      it('should NOT add event to span', () => {
+        const addEventSpy = sinon.spy();
+        const span = {
+          addEvent: addEventSpy,
+        } as unknown as tracing.Span;
+        const entries = {
+          [PTN.FETCH_START]: 123,
+        } as PerformanceEntries;
+
+        assert.strictEqual(addEventSpy.callCount, 0);
+
+        addSpanNetworkEvent(span, 'foo', entries);
+
+        assert.strictEqual(
+          addEventSpy.callCount,
+          0,
+          'should not call addEvent'
+        );
+      });
+    });
+  });
+  describe('getResource', () => {
+    const startTime = [0, 123123123] as HrTime;
+    beforeEach(() => {
+      const time = createHrTime(startTime, 500);
+      sinon.stub(performance, 'timeOrigin').value(0);
+      sinon.stub(performance, 'now').callsFake(() => hrTimeToNanoseconds(time));
+    });
+
+    describe('when resources are empty', () => {
+      it('should return undefined', () => {
+        const spanStartTime = createHrTime(startTime, 1);
+        const spanEndTime = createHrTime(startTime, 100);
+        const spanUrl = 'http://foo.com/bar.json';
+        const resources: PerformanceResourceTiming[] = [];
+
+        const resource = getResource(
+          spanUrl,
+          spanStartTime,
+          spanEndTime,
+          resources
+        );
+
+        assert.deepStrictEqual(
+          resource.mainRequest,
+          undefined,
+          'main request should be undefined'
+        );
+      });
+    });
+
+    describe('when resources has correct entry', () => {
+      it('should return the closest one', () => {
+        const spanStartTime = createHrTime(startTime, 1);
+        const spanEndTime = createHrTime(startTime, 402);
+        const spanUrl = 'http://foo.com/bar.json';
+        const resources: PerformanceResourceTiming[] = [];
+
+        // this one started earlier
+        resources.push(
+          createResource(
+            {
+              name: 'http://foo.com/bar.json',
+            },
+            createHrTime(startTime, -1),
+            100
+          )
+        );
+
+        // this one is correct
+        resources.push(
+          createResource(
+            {
+              name: 'http://foo.com/bar.json',
+            },
+            createHrTime(startTime, 1),
+            400
+          )
+        );
+
+        // this one finished after span
+        resources.push(
+          createResource(
+            {
+              name: 'http://foo.com/bar.json',
+            },
+            createHrTime(startTime, 1),
+            1000
+          )
+        );
+
+        const resource = getResource(
+          spanUrl,
+          spanStartTime,
+          spanEndTime,
+          resources
+        );
+
+        assert.deepStrictEqual(
+          resource.mainRequest,
+          resources[1],
+          'main request should be defined'
+        );
+      });
+      describe('But one resource has been already used', () => {
+        it('should return the next closest', () => {
+          const spanStartTime = createHrTime(startTime, 1);
+          const spanEndTime = createHrTime(startTime, 402);
+          const spanUrl = 'http://foo.com/bar.json';
+          const resources: PerformanceResourceTiming[] = [];
+
+          // this one started earlier
+          resources.push(
+            createResource(
+              {
+                name: 'http://foo.com/bar.json',
+              },
+              createHrTime(startTime, -1),
+              100
+            )
+          );
+
+          // this one is correct but ignored
+          resources.push(
+            createResource(
+              {
+                name: 'http://foo.com/bar.json',
+              },
+              createHrTime(startTime, 1),
+              400
+            )
+          );
+
+          // this one is also correct
+          resources.push(
+            createResource(
+              {
+                name: 'http://foo.com/bar.json',
+              },
+              createHrTime(startTime, 1),
+              300
+            )
+          );
+
+          // this one finished after span
+          resources.push(
+            createResource(
+              {
+                name: 'http://foo.com/bar.json',
+              },
+              createHrTime(startTime, 1),
+              1000
+            )
+          );
+
+          const ignoredResources = new WeakSet<PerformanceResourceTiming>();
+          ignoredResources.add(resources[1]);
+          const resource = getResource(
+            spanUrl,
+            spanStartTime,
+            spanEndTime,
+            resources,
+            ignoredResources
+          );
+
+          assert.deepStrictEqual(
+            resource.mainRequest,
+            resources[2],
+            'main request should be defined'
+          );
+        });
+      });
+    });
+
+    describe('when there are multiple resources from CorsPreflight requests', () => {
+      it('should return main request and cors preflight request', () => {
+        const spanStartTime = createHrTime(startTime, 1);
+        const spanEndTime = createHrTime(startTime, 182);
+        const spanUrl = 'http://foo.com/bar.json';
+        const resources: PerformanceResourceTiming[] = [];
+
+        // this one started earlier
+        resources.push(
+          createResource(
+            {
+              name: 'http://foo.com/bar.json',
+            },
+            createHrTime(startTime, 1),
+            10
+          )
+        );
+
+        // this one is correct
+        resources.push(
+          createResource(
+            {
+              name: 'http://foo.com/bar.json',
+            },
+            createHrTime(startTime, 1),
+            11
+          )
+        );
+
+        // this one finished after span
+        resources.push(
+          createResource(
+            {
+              name: 'http://foo.com/bar.json',
+            },
+            createHrTime(startTime, 50),
+            100
+          )
+        );
+
+        // this one finished after span
+        resources.push(
+          createResource(
+            {
+              name: 'http://foo.com/bar.json',
+            },
+            createHrTime(startTime, 50),
+            130
+          )
+        );
+
+        const resource = getResource(
+          spanUrl,
+          spanStartTime,
+          spanEndTime,
+          resources,
+          undefined
+        );
+
+        assert.deepStrictEqual(
+          resource.corsPreFlightRequest,
+          resources[0],
+          'cors preflight request should be defined'
+        );
+
+        assert.deepStrictEqual(
+          resource.mainRequest,
+          resources[3],
+          'main request should be defined'
+        );
+      });
+    });
+  });
+
+  describe('shouldPropagateTraceHeaders', () => {
+    it('should propagate trace when url is the same as origin', () => {
+      const result = shouldPropagateTraceHeaders(
+        `${globalThis.location.origin}/foo/bar`
+      );
+      assert.strictEqual(result, true);
+    });
+    it('should propagate trace when url match', () => {
+      const result = shouldPropagateTraceHeaders(
+        'http://foo.com',
+        'http://foo.com'
+      );
+      assert.strictEqual(result, true);
+    });
+    it('should propagate trace when url match regexp', () => {
+      const result = shouldPropagateTraceHeaders('http://foo.com', /foo.+/);
+      assert.strictEqual(result, true);
+    });
+    it('should propagate trace when url match array of string', () => {
+      const result = shouldPropagateTraceHeaders('http://foo.com', [
+        'http://foo.com',
+      ]);
+      assert.strictEqual(result, true);
+    });
+    it('should propagate trace when url match array of regexp', () => {
+      const result = shouldPropagateTraceHeaders('http://foo.com', [/foo.+/]);
+      assert.strictEqual(result, true);
+    });
+    it("should NOT propagate trace when url doesn't match", () => {
+      const result = shouldPropagateTraceHeaders('http://foo.com');
+      assert.strictEqual(result, false);
+    });
+  });
+
+  describe('parseUrl', () => {
+    const urlFields: Array<keyof URLLike> = [
+      'hash',
+      'host',
+      'hostname',
+      'href',
+      'origin',
+      'password',
+      'pathname',
+      'port',
+      'protocol',
+      'search',
+      'username',
+    ];
+    it('should parse url', () => {
+      const url = parseUrl('https://opentelemetry.io/foo');
+      urlFields.forEach(field => {
+        assert.strictEqual(typeof url[field], 'string');
+      });
+    });
+
+    it('should parse relative url', () => {
+      const url = parseUrl('/foo');
+      urlFields.forEach(field => {
+        assert.strictEqual(typeof url[field], 'string');
+      });
+    });
+  });
+
+  describe('normalizeUrl', () => {
+    it('should normalize url', () => {
+      const url = normalizeUrl('https://opentelemetry.io/你好');
+      assert.strictEqual(url, 'https://opentelemetry.io/%E4%BD%A0%E5%A5%BD');
+    });
+
+    it('should normalize relative url', () => {
+      const url = normalizeUrl('/你好');
+      const urlObj = new URL(url);
+      assert.strictEqual(urlObj.pathname, '/%E4%BD%A0%E5%A5%BD');
+    });
+  });
+});
