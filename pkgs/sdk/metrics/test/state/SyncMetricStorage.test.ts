15a7b91892da1f297e9f5a333eaa5dc3da10cc27
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2023-11-09 23:45:01 +0000

    [AutoMerge] staging @ [2cd0511...](https://github.com/open-telemetry/opentelemetry-sandbox-web-js/commit/2cd0511c6266bd2edc49e8cfb89f6c566d79fc27)
    ### Moving package from auto-merge/js/api to pkgs/api/
    ### Moving package from auto-merge/js/packages/opentelemetry-semantic-conventions/ to pkgs/semantic-conventions/
    ### Moving package from auto-merge/js/packages/opentelemetry-core/ to pkgs/core/
    ### Moving package from auto-merge/js/packages/opentelemetry-resources/ to pkgs/resources/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone-peer-dep/ to pkgs/context/zone-peer-dep/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone/ to pkgs/context/zone/
    ### Moving package from auto-merge/js/packages/opentelemetry-propagator-b3/ to pkgs/propagators/b3/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-base/ to pkgs/sdk/trace/base/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-web/ to pkgs/sdk/trace/web/
    ### Moving package from auto-merge/js/packages/sdk-metrics/ to pkgs/sdk/metrics/
    ### Moving package from auto-merge/js/experimental/packages/sdk-logs/ to pkgs/sdk/logs/
    ### Moving package from auto-merge/js/experimental/packages/otlp-exporter-base to pkgs/exporters/otlp/base/
    ### Moving package from auto-merge/js/experimental/packages/otlp-transformer to pkgs/exporters/otlp/transformer
    ### Moving package from auto-merge/js/experimental/packages/otlp-proto-exporter-base to pkgs/exporters/otlp/proto-base
    ### Moving package from auto-merge/js/packages/opentelemetry-exporter-zipkin/ to pkgs/exporters/zipkin/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-proto to pkgs/exporters/otlp/trace/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-http to pkgs/exporters/otlp/trace/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-exporter-metrics-otlp-http to pkgs/exporters/otlp/metrics/http/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-proto to pkgs/exporters/otlp/logs/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-http to pkgs/exporters/otlp/logs/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation/ to pkgs/instrumentations/instrumentation/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-fetch/ to pkgs/instrumentations/web/fetch
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-xml-http-request/ to pkgs/instrumentations/web/xhr
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-document-load/ to pkgs/instrumentations/web/document-load/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-user-interaction/ to pkgs/instrumentations/web/user-interaction/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-long-task/ to pkgs/instrumentations/web/long-task/
    ### Moving package from auto-merge/contrib/metapackages/auto-instrumentations-web/ to pkgs/instrumentations/web/auto/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-browser-detector/ to pkgs/detectors/browser/
    ### Moving package from auto-merge/js/experimental/packages/api-logs/ to pkgs/api-logs/
    ### Moving package from auto-merge/js/experimental/packages/api-events/ to pkgs/api-events/
    ### Moving package from auto-merge/js/examples/opentelemetry-web/ to examples/web/js/

---
+++ pkgs/sdk/metrics/test/state/SyncMetricStorage.test.ts
@@ -0,0 +1,138 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as api from '@opentelemetry/sandbox-api';
+import * as assert from 'assert';
+
+import { SumAggregator } from '../../src/aggregator';
+import { AggregationTemporality } from '../../src/export/AggregationTemporality';
+import { DataPointType } from '../../src/export/MetricData';
+import { MetricCollectorHandle } from '../../src/state/MetricCollector';
+import { SyncMetricStorage } from '../../src/state/SyncMetricStorage';
+import { NoopAttributesProcessor } from '../../src/view/AttributesProcessor';
+import {
+  assertMetricData,
+  assertDataPoint,
+  commonAttributes,
+  commonValues,
+  defaultInstrumentDescriptor,
+} from '../util';
+
+const deltaCollector: MetricCollectorHandle = {
+  selectAggregationTemporality: () => AggregationTemporality.DELTA,
+};
+
+const cumulativeCollector: MetricCollectorHandle = {
+  selectAggregationTemporality: () => AggregationTemporality.CUMULATIVE,
+};
+
+describe('SyncMetricStorage', () => {
+  describe('record', () => {
+    it('no exceptions on record', () => {
+      const metricStorage = new SyncMetricStorage(
+        defaultInstrumentDescriptor,
+        new SumAggregator(true),
+        new NoopAttributesProcessor(),
+        []
+      );
+
+      for (const value of commonValues) {
+        for (const attributes of commonAttributes) {
+          metricStorage.record(value, attributes, api.context.active(), [0, 0]);
+        }
+      }
+    });
+  });
+
+  describe('collect', () => {
+    describe('Delta Collector', () => {
+      it('should collect and reset memos', async () => {
+        const metricStorage = new SyncMetricStorage(
+          defaultInstrumentDescriptor,
+          new SumAggregator(true),
+          new NoopAttributesProcessor(),
+          [deltaCollector]
+        );
+
+        metricStorage.record(1, {}, api.context.active(), [0, 0]);
+        metricStorage.record(2, {}, api.context.active(), [1, 1]);
+        metricStorage.record(3, {}, api.context.active(), [2, 2]);
+        {
+          const metric = metricStorage.collect(deltaCollector, [3, 3]);
+
+          assertMetricData(metric, DataPointType.SUM);
+          assert.strictEqual(metric.dataPoints.length, 1);
+          assertDataPoint(metric.dataPoints[0], {}, 6, [0, 0], [3, 3]);
+        }
+
+        // The attributes should not be memorized.
+        {
+          const metric = metricStorage.collect(deltaCollector, [4, 4]);
+
+          assert.strictEqual(metric, undefined);
+        }
+
+        metricStorage.record(1, {}, api.context.active(), [5, 5]);
+        {
+          const metric = metricStorage.collect(deltaCollector, [6, 6]);
+
+          assertMetricData(metric, DataPointType.SUM);
+          assert.strictEqual(metric.dataPoints.length, 1);
+          assertDataPoint(metric.dataPoints[0], {}, 1, [5, 5], [6, 6]);
+        }
+      });
+    });
+
+    describe('Cumulative Collector', () => {
+      it('should collect cumulative metrics', async () => {
+        const metricStorage = new SyncMetricStorage(
+          defaultInstrumentDescriptor,
+          new SumAggregator(true),
+          new NoopAttributesProcessor(),
+          [cumulativeCollector]
+        );
+        metricStorage.record(1, {}, api.context.active(), [0, 0]);
+        metricStorage.record(2, {}, api.context.active(), [1, 1]);
+        metricStorage.record(3, {}, api.context.active(), [2, 2]);
+        {
+          const metric = metricStorage.collect(cumulativeCollector, [3, 3]);
+
+          assertMetricData(metric, DataPointType.SUM);
+          assert.strictEqual(metric.dataPoints.length, 1);
+          assertDataPoint(metric.dataPoints[0], {}, 6, [0, 0], [3, 3]);
+        }
+
+        // The attributes should be memorized.
+        {
+          const metric = metricStorage.collect(cumulativeCollector, [4, 4]);
+
+          assertMetricData(metric, DataPointType.SUM);
+          assert.strictEqual(metric.dataPoints.length, 1);
+          assertDataPoint(metric.dataPoints[0], {}, 6, [0, 0], [4, 4]);
+        }
+
+        metricStorage.record(1, {}, api.context.active(), [5, 5]);
+        {
+          const metric = metricStorage.collect(cumulativeCollector, [6, 6]);
+
+          assertMetricData(metric, DataPointType.SUM);
+          assert.strictEqual(metric.dataPoints.length, 1);
+          assertDataPoint(metric.dataPoints[0], {}, 7, [0, 0], [6, 6]);
+        }
+      });
+    });
+  });
+});
