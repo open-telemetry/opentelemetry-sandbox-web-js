ff0468842f9c9ebf34cd5f0025a284dac90f8d90
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2023-11-17 22:51:34 +0000

    [AutoMerge] staging @ [9c5bcd5...](https://github.com/open-telemetry/opentelemetry-sandbox-web-js/commit/9c5bcd58889efaffcd36a69c34cf0290f8eb39b9)
    ### Moving package from auto-merge/js/api to pkgs/api/
    ### Moving package from auto-merge/js/packages/opentelemetry-semantic-conventions/ to pkgs/semantic-conventions/
    ### Moving package from auto-merge/js/packages/opentelemetry-core/ to pkgs/core/
    ### Moving package from auto-merge/js/packages/opentelemetry-resources/ to pkgs/resources/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone-peer-dep/ to pkgs/context/zone-peer-dep/
    ### Moving package from auto-merge/js/packages/opentelemetry-context-zone/ to pkgs/context/zone/
    ### Moving package from auto-merge/js/packages/opentelemetry-propagator-b3/ to pkgs/propagators/b3/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-base/ to pkgs/sdk/trace/base/
    ### Moving package from auto-merge/js/packages/opentelemetry-sdk-trace-web/ to pkgs/sdk/trace/web/
    ### Moving package from auto-merge/js/packages/sdk-metrics/ to pkgs/sdk/metrics/
    ### Moving package from auto-merge/js/experimental/packages/sdk-logs/ to pkgs/sdk/logs/
    ### Moving package from auto-merge/js/experimental/packages/otlp-exporter-base to pkgs/exporters/otlp/base/
    ### Moving package from auto-merge/js/experimental/packages/otlp-transformer to pkgs/exporters/otlp/transformer
    ### Moving package from auto-merge/js/experimental/packages/otlp-proto-exporter-base to pkgs/exporters/otlp/proto-base
    ### Moving package from auto-merge/js/packages/opentelemetry-exporter-zipkin/ to pkgs/exporters/zipkin/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-proto to pkgs/exporters/otlp/trace/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-trace-otlp-http to pkgs/exporters/otlp/trace/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-exporter-metrics-otlp-http to pkgs/exporters/otlp/metrics/http/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-proto to pkgs/exporters/otlp/logs/proto/
    ### Moving package from auto-merge/js/experimental/packages/exporter-logs-otlp-http to pkgs/exporters/otlp/logs/http/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation/ to pkgs/instrumentations/instrumentation/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-fetch/ to pkgs/instrumentations/web/fetch
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-instrumentation-xml-http-request/ to pkgs/instrumentations/web/xhr
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-document-load/ to pkgs/instrumentations/web/document-load/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-user-interaction/ to pkgs/instrumentations/web/user-interaction/
    ### Moving package from auto-merge/contrib/plugins/web/opentelemetry-instrumentation-long-task/ to pkgs/instrumentations/web/long-task/
    ### Moving package from auto-merge/contrib/metapackages/auto-instrumentations-web/ to pkgs/instrumentations/web/auto/
    ### Moving package from auto-merge/js/experimental/packages/opentelemetry-browser-detector/ to pkgs/detectors/browser/
    ### Moving package from auto-merge/js/experimental/packages/api-logs/ to pkgs/api-logs/
    ### Moving package from auto-merge/js/experimental/packages/api-events/ to pkgs/api-events/
    ### Moving package from auto-merge/js/examples/opentelemetry-web/ to examples/web/js/

---
+++ pkgs/exporters/otlp/transformer/src/metrics/internal.ts
@@ -0,0 +1,208 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { OtlpEncodingOptions } from '../common/types';
+import { ValueType } from '@opentelemetry/sandbox-api';
+import {
+  AggregationTemporality,
+  DataPoint,
+  DataPointType,
+  ExponentialHistogram,
+  Histogram,
+  MetricData,
+  ResourceMetrics,
+  ScopeMetrics,
+} from '@opentelemetry/sandbox-sdk-metrics';
+import { toAttributes } from '../common/internal';
+import {
+  EAggregationTemporality,
+  IExponentialHistogramDataPoint,
+  IHistogramDataPoint,
+  IMetric,
+  INumberDataPoint,
+  IResourceMetrics,
+  IScopeMetrics,
+} from './types';
+import { Encoder, getOtlpEncoder } from '../common';
+
+export function toResourceMetrics(
+  resourceMetrics: ResourceMetrics,
+  options?: OtlpEncodingOptions
+): IResourceMetrics {
+  const encoder = getOtlpEncoder(options);
+  return {
+    resource: {
+      attributes: toAttributes(resourceMetrics.resource.attributes),
+      droppedAttributesCount: 0,
+    },
+    schemaUrl: undefined,
+    scopeMetrics: toScopeMetrics(resourceMetrics.scopeMetrics, encoder),
+  };
+}
+
+export function toScopeMetrics(
+  scopeMetrics: ScopeMetrics[],
+  encoder: Encoder
+): IScopeMetrics[] {
+  return Array.from(
+    scopeMetrics.map(metrics => ({
+      scope: {
+        name: metrics.scope.name,
+        version: metrics.scope.version,
+      },
+      metrics: metrics.metrics.map(metricData => toMetric(metricData, encoder)),
+      schemaUrl: metrics.scope.schemaUrl,
+    }))
+  );
+}
+
+export function toMetric(metricData: MetricData, encoder: Encoder): IMetric {
+  const out: IMetric = {
+    name: metricData.descriptor.name,
+    description: metricData.descriptor.description,
+    unit: metricData.descriptor.unit,
+  };
+
+  const aggregationTemporality = toAggregationTemporality(
+    metricData.aggregationTemporality
+  );
+
+  switch (metricData.dataPointType) {
+    case DataPointType.SUM:
+      out.sum = {
+        aggregationTemporality,
+        isMonotonic: metricData.isMonotonic,
+        dataPoints: toSingularDataPoints(metricData, encoder),
+      };
+      break;
+    case DataPointType.GAUGE:
+      out.gauge = {
+        dataPoints: toSingularDataPoints(metricData, encoder),
+      };
+      break;
+    case DataPointType.HISTOGRAM:
+      out.histogram = {
+        aggregationTemporality,
+        dataPoints: toHistogramDataPoints(metricData, encoder),
+      };
+      break;
+    case DataPointType.EXPONENTIAL_HISTOGRAM:
+      out.exponentialHistogram = {
+        aggregationTemporality,
+        dataPoints: toExponentialHistogramDataPoints(metricData, encoder),
+      };
+      break;
+  }
+
+  return out;
+}
+
+function toSingularDataPoint(
+  dataPoint:
+    | DataPoint<number>
+    | DataPoint<Histogram>
+    | DataPoint<ExponentialHistogram>,
+  valueType: ValueType,
+  encoder: Encoder
+) {
+  const out: INumberDataPoint = {
+    attributes: toAttributes(dataPoint.attributes),
+    startTimeUnixNano: encoder.encodeHrTime(dataPoint.startTime),
+    timeUnixNano: encoder.encodeHrTime(dataPoint.endTime),
+  };
+
+  switch (valueType) {
+    case ValueType.INT:
+      out.asInt = dataPoint.value as number;
+      break;
+    case ValueType.DOUBLE:
+      out.asDouble = dataPoint.value as number;
+      break;
+  }
+
+  return out;
+}
+
+function toSingularDataPoints(
+  metricData: MetricData,
+  encoder: Encoder
+): INumberDataPoint[] {
+  return metricData.dataPoints.map(dataPoint => {
+    return toSingularDataPoint(
+      dataPoint,
+      metricData.descriptor.valueType,
+      encoder
+    );
+  });
+}
+
+function toHistogramDataPoints(
+  metricData: MetricData,
+  encoder: Encoder
+): IHistogramDataPoint[] {
+  return metricData.dataPoints.map(dataPoint => {
+    const histogram = dataPoint.value as Histogram;
+    return {
+      attributes: toAttributes(dataPoint.attributes),
+      bucketCounts: histogram.buckets.counts,
+      explicitBounds: histogram.buckets.boundaries,
+      count: histogram.count,
+      sum: histogram.sum,
+      min: histogram.min,
+      max: histogram.max,
+      startTimeUnixNano: encoder.encodeHrTime(dataPoint.startTime),
+      timeUnixNano: encoder.encodeHrTime(dataPoint.endTime),
+    };
+  });
+}
+
+function toExponentialHistogramDataPoints(
+  metricData: MetricData,
+  encoder: Encoder
+): IExponentialHistogramDataPoint[] {
+  return metricData.dataPoints.map(dataPoint => {
+    const histogram = dataPoint.value as ExponentialHistogram;
+    return {
+      attributes: toAttributes(dataPoint.attributes),
+      count: histogram.count,
+      min: histogram.min,
+      max: histogram.max,
+      sum: histogram.sum,
+      positive: {
+        offset: histogram.positive.offset,
+        bucketCounts: histogram.positive.bucketCounts,
+      },
+      negative: {
+        offset: histogram.negative.offset,
+        bucketCounts: histogram.negative.bucketCounts,
+      },
+      scale: histogram.scale,
+      zeroCount: histogram.zeroCount,
+      startTimeUnixNano: encoder.encodeHrTime(dataPoint.startTime),
+      timeUnixNano: encoder.encodeHrTime(dataPoint.endTime),
+    };
+  });
+}
+
+function toAggregationTemporality(
+  temporality: AggregationTemporality
+): EAggregationTemporality {
+  switch (temporality) {
+    case AggregationTemporality.DELTA:
+      return EAggregationTemporality.AGGREGATION_TEMPORALITY_DELTA;
+    case AggregationTemporality.CUMULATIVE:
+      return EAggregationTemporality.AGGREGATION_TEMPORALITY_CUMULATIVE;
+  }
+}
