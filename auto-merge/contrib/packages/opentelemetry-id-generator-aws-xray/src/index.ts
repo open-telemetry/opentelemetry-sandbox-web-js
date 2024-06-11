89e0b9a6ae50de319b93e6fb58c0502a58034728
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2024-06-11 16:29:17 +0000

    [AutoMerge] Identifying and fixing merge issues from staged repos Processing otel-js  - (xM) /auto-merge/js/.markdownlint-cli2.jsonc - Re-Copying master file  - (xM) /auto-merge/js/experimental/examples/events/README.md - Re-Copying master file  - (xM) /auto-merge/js/experimental/examples/events/index.ts - Re-Copying master file  - (xM) /auto-merge/js/experimental/examples/events/package.json - Re-Copying master file  - (xM) /auto-merge/js/experimental/examples/events/tsconfig.json - Re-Copying master file  - (xS) /auto-merge/js/experimental/packages/api-logs/src/platform/index.ts - Re-Copying master file as size mismatch 652 !== 631  - (*F) /auto-merge/js/experimental/packages/otlp-grpc-exporter-base/src/generated - Removing extra folder generated  - (*E) /auto-merge/js/experimental/packages/otlp-grpc-exporter-base/src/internal-types.ts - Removing extra file  - (*E) /auto-merge/js/experimental/packages/otlp-grpc-exporter-base/src/serializers.ts - Removing extra file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/.eslintignore - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/.eslintrc.js - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/.npmignore - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/LICENSE - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/README.md - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/package.json - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/src/AWSXRayLambdaPropagator.ts - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/src/index.ts - Re-Copying master file  - (xM) /auto-merge/js/experimental/packages/propagator-aws-xray-lambda/test/AWSXRayLambdaPropagator.test.ts - Re-Copying master file ...truncated... Processing otel-js-contrib

--- auto-merge/contrib/packages/opentelemetry-id-generator-aws-xray/src/index.ts
+++ auto-merge/contrib/packages/opentelemetry-id-generator-aws-xray/src/index.ts
@@ -13,12 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export {
-  hostDetector,
-  hostDetectorSync,
-  osDetector,
-  osDetectorSync,
-  processDetector,
-  processDetectorSync,
-  serviceInstanceIdDetectorSync,
-} from './node';
+export * from './platform';
--- auto-merge/js/experimental/packages/api-logs/src/platform/index.ts
+++ auto-merge/js/experimental/packages/api-logs/src/platform/index.ts
@@ -14,4 +14,4 @@
  * limitations under the License.
  */
 
-export { defaultServiceName } from './node';
+export * from './node';
